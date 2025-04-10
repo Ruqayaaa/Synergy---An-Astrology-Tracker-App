9b48e6653c2b47d92a72a6182be219254eb47493
Zain Alhashemi <91572978+zalhashemi@users.noreply.github.com>
2025-04-10 23:35:51 +0300

    Create HoroscopeView.swift

---
+++ HoroscopeView.swift
@@ -0,0 +1,9 @@
+//this page is to showcase the user's horoscope for the day, week, and month
+import SwiftUI
+
+struct HoroscopeView: View {
+    var body: some View {
+        VStack(spacing: 20) {
+        }
+    }
+}
